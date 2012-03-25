2b139f6c5aa65a2cad5fc330802674a87c62230e
Sean Behan <inbox@seanbehan.com>
2012-03-25 11:21:44 -0400

    ruby extensions init

---
+++ README.md
@@ -0,0 +1,37 @@
+# Rails Extensions
+
+## Installation
+
+Place in your Gemfile and run bundle
+
+``` ruby
+
+gem "rails_extensions", git: "git@github.com:AgilionApps/rails_extensions.git"
+
+```
+
+## String Class Extensions
+
+``` ruby
+
+ # Hash a string
+ "password".to_sha1
+ # => "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"
+
+ # Extract email addresses from a string
+ "Hello sean@agilionapps.com, how are you?".to_email
+ # => ["sean@agilionapps.com"]
+
+ # Turn a string into a url friendly slug
+ "hello world how are you today?".to_slug
+ # => "hello-world-how-are-you-today"
+
+```
+
+## Testing
+
+At least for now...
+
+``` ruby
+ruby -Itest test/string.rb
+```
---
+++ lib/rails_extensions/string.rb
@@ -0,0 +1,33 @@
+module RailsExtensions
+  module String
+    def to_sha1
+      Digest::SHA1.hexdigest(self)
+    end
+
+    def to_slug(delimiter="-")
+      self.gsub(/[^a-z0-9]/i, " ").strip.gsub(/\s/, delimiter).gsub(/#{delimiter}{2}/, delimiter).downcase
+    end
+
+    def to_alpha_numeric
+      self.gsub(/[^a-z0-9]/i, " ").gsub(/\s{2,}/, ' ').strip
+    end
+
+    def to_email
+      self.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)
+    end
+
+    def mentions
+      self.scan(/@[a-z_]+\b/)
+    end
+
+    def hash_tags
+      self.scan(/#[a-z_]+\b/)
+    end
+
+    def escape_acronyms(regex)
+      self.gsub(regex) do |str|
+        str.upcase
+      end
+    end
+  end
+end
